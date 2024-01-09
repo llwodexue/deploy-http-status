9b95cecc580eaad6399bcdaeb14818820b6355b0
lyn <Lyndon_lilin@123.com>
2024-01-09 16:16:40 +0800

    feat: helmet safe

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const Koa = require('koa')
 const cors = require('koa-cors')
 const KoaStatic = require('koa-static')
+const helmet = require('koa-helmet')
 const router = require('./router')
 const bodyParser = require('koa-bodyparser')
 const { systemLogger } = require('./middleware/logger')
@@ -9,15 +10,24 @@ const { systemLogger } = require('./middleware/logger')
 const app = new Koa()
 
 /** static */
-app.use(new KoaStatic(__dirname + "/public"));
+app.use(new KoaStatic(__dirname + '/public'))
 
 /** request */
 app.use(cors({ origin: true, credentials: true }))
 app.use(bodyParser()) // post body
+app.use(helmet())
 
 /** router */
 app.use(router.routes())
 app.use(router.allowedMethods()) // 405 Method Not Allowed
+
+/** socketIo */
+const http = require('http').Server(app.callback())
+const socketIo = require('socket.io')(http)
+socketIo.on('connection', socket => {
+  console.log(socket)
+  systemLogger.info('socketIo user connected')
+})
 
 /** server */
 const server = app.listen(3007, () => {
