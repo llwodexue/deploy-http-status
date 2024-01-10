94d6368eb0e687e5b72ff46912caed6c6b5943e6
lyn <Lyndon_lilin@123.com>
2024-01-10 10:46:08 +0800

    feat: socket error handler

--- websocket/index.js
+++ websocket/index.js
@@ -2,10 +2,18 @@ const { systemLogger } = require('../middleware/logger')
 
 const listenAndEmitInfo = async io => {
   io.on('connection', socket => {
+    systemLogger.info('io is connected')
     socket.on('message', data => {
       console.log('message from client: ', data)
     })
-    systemLogger.info('io is connected')
+
+    /** error handler */
+    socket.use(([events], next) => {
+      if (/error/gi.test(events)) return next(new Error(events))
+      next()
+    })
+    socket.on('error', err => systemLogger.error(err))
+    socket.on('disconnect', () => systemLogger.info('io is disconnected'))
   })
 }
 
