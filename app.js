3ff189376b00a714a2ed29229007f68c02c5f517
lyn <Lyndon_lilin@123.com>
2024-03-20 09:45:53 +0800

    refactor: module

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ listenAndEmitInfo(io)
 
 /** server */
 const port = 3007
-const nginxPort = 8090
+const nginxPort = 8010
 server.listen(port) // need server listen not app listen
 systemLogger.info('running on http://127.0.0.1:%s', port)
 systemLogger.info('please click it, nginx start on https://127.0.0.1:%s', nginxPort)
